6b429f0e59854f130cbd61982646514f9a119f80
shay7210 <shay7210@gmail.com>
2024-10-28 19:23:29 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Array from './Array files/Array'
+import Cell from "./Array files/cell";
+
+function App() {
+    return (<>
+            <head>
+                <meta charSet="utf-8" />
+                <title>Array</title>
+            </head>
+
+            <Array/>
+    </>
+
+    );
+}
+export default App;
